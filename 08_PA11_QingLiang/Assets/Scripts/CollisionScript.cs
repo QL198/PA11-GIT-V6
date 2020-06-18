b2a395dfbd162fe5fd08f62acc79fd605661385e
QL198 <64115131+QL198@users.noreply.github.com>
2020-06-18 17:49:50 +0800

    Done by QingLiang

---
+++ 08_PA11_QingLiang/Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionScript : MonoBehaviour
+{
+    public Text Score_Txt;
+    public float Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            
+            Score++;
+            Score_Txt.text = "Score : " + Score;
+            Destroy(collision.gameObject);
+        }
+    }
+}
